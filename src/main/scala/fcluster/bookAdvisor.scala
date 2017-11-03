1ae18097ee5b4403b6da57f062ebb56ef912b6ea
yesidbotero <yesidboterom@gmail.com>
2017-11-03 07:56:50 -0500

    creating first cluster

---
+++ src/main/scala/fcluster/bookAdvisor.scala
@@ -0,0 +1,8 @@
+package fcluster
+
+import akka.actor.{Actor, ActorLogging}
+
+class bookAdvisor extends Actor with ActorLogging {
+
+  override def receive = ???
+}
---
+++ src/main/scala/fcluster/bookAdvisorApp.scala
@@ -0,0 +1,8 @@
+package fcluster
+
+class bookAdvisorApp {
+
+  def main(args: Array[String]) = {
+
+  }
+}
